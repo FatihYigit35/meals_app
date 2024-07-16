ddfe0678392c1c93cb9b52c8692145e7c2fbb5dd
FatihYigit35 <izmirsoftware.com.tr@gmail.com>
2024-07-17 01:22:08 +0300

    The screen where the dishes will be listed

---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemBuilder: (context, index) => Text(meals[index].title),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('No meal found!'),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Try selecting a different category!',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            )
+          ],
+        ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
+}
