2ebfb55c0a8d3e87ee311fe1b58543d9149a47bf
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-08-04 08:15:22 +0300

    converted to statefulwidget for animation
    Co-Authored-By: izmirsoftware <168662977+izmirsoftware@users.noreply.github.com>

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,7 +6,7 @@ import 'package:meals_app/widgets/category_grid_item.dart';
 
 import '../models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
     required this.availableMeals,
@@ -14,8 +14,32 @@ class CategoriesScreen extends StatelessWidget {
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final mealList = availableMeals
+    final mealList = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
