da68b93ff36ca16acdff21493b1d7b5eb61185b5
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-08-04 17:42:31 +0300

    Added animation to images during transitions between meals and meal details
    Co-Authored-By: izmirsoftware <168662977+izmirsoftware@users.noreply.github.com>

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -57,10 +57,13 @@ class MealDetailsScreen extends ConsumerWidget {
           padding: const EdgeInsets.only(bottom: 10),
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(
                 height: 16,
